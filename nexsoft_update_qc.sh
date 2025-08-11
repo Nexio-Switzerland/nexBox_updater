1d0b589973fb51a5daf97b8ea52c971ea6f089b5
Kevin <97556752+Kevin-Nexio@users.noreply.github.com>
2025-08-11 17:48:10 +0200

    Update nexsoft_update_qc.sh

--- nexsoft_update_qc.sh
+++ nexsoft_update_qc.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 #######################################
 # CONFIG
 #######################################
-DOWNLOAD_URL="${DOWNLOAD_URL:-https://dl-s3cy4u5n.swisstransfer.com/api/download/9b8b27ec-00cb-4990-8ba9-8bde8d24d250/8d49e288-bb4f-44ef-9174-d2e5dc1dc2fe}"   # <- à adapter
+DOWNLOAD_URL="${DOWNLOAD_URL:-https://dl-s3cy4u5n.swisstransfer.com/api/download/884d8cd5-a51b-42c6-8017-b98fabdccdac/e1a3d220-245d-416e-aad5-1c0e2c82c4f6}"   # <- à adapter
 # HTTP download tuning (UA/Referer). Set DOWNLOAD_REFERER for hosts like SwissTransfer
 DOWNLOAD_UA="${DOWNLOAD_UA:-Mozilla/5.0}"
 DOWNLOAD_REFERER="${DOWNLOAD_REFERER:-}"
